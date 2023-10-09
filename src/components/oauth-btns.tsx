df8ac99e330a47c01d39a6c7049dc1a56f6c4126
jilv220 <hkfhfkcoc@protonmail.com>
2023-10-08 17:01:21 -0700

    implement all oauth providers

--- src/components/oauth-btns.tsx
+++ src/components/oauth-btns.tsx
@@ -3,6 +3,9 @@
 import { signIn, useSession } from "next-auth/react";
 import { Button } from "./ui/button";
 import { redirect } from "next/navigation";
+import { Github, Twitter } from "lucide-react";
+import Google from "./brand-icons/google-icon";
+import Discord from "./brand-icons/discord-icon";
 
 export default function OAuthBtns() {
   const { data: session, status } = useSession();
@@ -12,8 +15,19 @@ export default function OAuthBtns() {
   }
 
   return (
-    <Button onClick={() => signIn("github", { callbackUrl: "/blog" })}>
-      Sign in with Github
-    </Button>
+    <div className="flex flex-col gap-2 w-full">
+      <Button onClick={() => signIn("github", { callbackUrl: "/blog" })}>
+        <Github className="mr-2 h-4 w-4" />
+        Sign in with Github
+      </Button>
+      <Button onClick={() => signIn("google", { callbackUrl: "/blog" })}>
+        <Google className="mr-2 h-4 w-4" />
+        Sign in with Google
+      </Button>
+      <Button onClick={() => signIn("discord", { callbackUrl: "/blog" })}>
+        <Discord className="mr-2 h-4 w-4" />
+        Sign in with Discord
+      </Button>
+    </div>
   );
 }
--- src/config/auth-options.ts
+++ src/config/auth-options.ts
@@ -1,6 +1,8 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db/drizzle";
 
@@ -10,6 +12,14 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
     }),
     // CredentialsProvider({
     //   name: "Credentials",
