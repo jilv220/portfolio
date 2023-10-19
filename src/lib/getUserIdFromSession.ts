7d7bc99f1cbb48af61be86997283c96c51086895
jilv220 <hkfhfkcoc@protonmail.com>
2023-10-18 20:27:46 -0700

    fix getUserIdFromSession

--- src/lib/getUserIdFromSession.ts
+++ src/lib/getUserIdFromSession.ts
@@ -1,20 +1,11 @@
-import { db } from "@/db/drizzle";
-import { sessions } from "@/db/schema/sessions";
-import { eq } from "drizzle-orm";
-import { cookies } from "next/headers";
+import { authOptions } from "@/config/auth-options";
+import { getServerSession } from "next-auth";
 
 export async function getUserIdFromSession() {
-  const cookieStore = cookies();
-  const token = cookieStore.get("next-auth.session-token");
-
-  if (!token) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
     return undefined;
   }
 
-  const [user] = await db
-    .select({ userId: sessions.userId })
-    .from(sessions)
-    .where(eq(sessions.sessionToken, token.value));
-
-  return user.userId;
+  return session.user.id;
 }
