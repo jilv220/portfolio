7502fad4f35569a4a7694749075e939e2e350872
Lyu Ji <hkfhfkcoc@protonmail.com>
2024-12-17 20:07:56 -0600

    test cache burst

--- src/app/(default)/resume/page.tsx
+++ src/app/(default)/resume/page.tsx
@@ -1,11 +1,10 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Resume",
+	title: "Resume",
 };
 
 export default function Resume() {
-  return (
-    <iframe className="h-screen" src='resume.pdf' />
-  )
+	const tz = Date.now();
+	return <iframe className="h-screen" src={`resume.pdf?v=${tz}`} />;
 }
